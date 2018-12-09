1b72511fdcc8ec6d28ce0002b16222f92fa438c5
renjer <mranjbar.z2993@gmail.com>
2018-12-09 17:08:59 +0330

    Add mellat bank

--- test/sepah/sepah.js
+++ test/sepah/sepah.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const sepah = require('../../lib/deposit-to-iban').sepah
+const mellat = require('../../lib/deposit-to-iban').mellat
 describe('testing convertSepahDepositToIban(deposit) function ', function() {
   it('Should calculate iban correctly', () => {
     const deposit = '1426304971108'
@@ -18,6 +19,14 @@ describe('testing isIbanFromSepah(iban) function ', function() {
 
   it('should return false', () => {
     const iban = 'IR840620000001426304971108'
-    assert.equal( sepah.isIbanFromSepah(iban), false);
+    assert.equal( sepah.isIbanFromThisBank(iban), false);
   });
 });
+
+// describe('testing convertMellatDepositToIban(deposit) function ', function() {
+//   it('Should calculate iban correctly', () => {
+//     const deposit = '4168450796'
+//     const iban = 'IR690120010000004168450796'
+//     assert.equal(mellat.convertDepositToIban(deposit), iban);
+//   });
+// });
