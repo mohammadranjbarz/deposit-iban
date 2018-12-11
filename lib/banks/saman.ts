9b026eaf0660b46c0e5222f0159b0a62cced2a9d
renjer <mranjbar.z2993@gmail.com>
2018-12-11 11:29:01 +0330

    remove extra checking in saman

--- lib/banks/saman.ts
+++ lib/banks/saman.ts
@@ -2,37 +2,33 @@ import * as utils from '../utils'
 import {isSourceOfIbanIsValid} from '../utils'
 
 export class Saman {
-    bankCode: string
+  bankCode: string
 
-    public constructor(bankCode: string) {
-        this.bankCode = bankCode;
-    }
+  public constructor(bankCode: string) {
+    this.bankCode = bankCode;
+  }
 
-    convertDepositToIban(deposit :string):string {
-        let formattedBankCode = this.bankCode;
-        // @ts-ignore:
-        if (formattedBankCode.startsWith('0')) {
-            formattedBankCode = formattedBankCode.replace('0', '');
-        }
-        let bban;
-        if (deposit.indexOf('-') > -1) {
-            const parts = deposit.split('-');
-            if (parts.length !== 4) throw new Error('Invalid deposit');
-            const part0 = utils.addPadString(parts[0], '0', 4);
-            const part1 = utils.addPadString(parts[1], '0', 3);
-            const part2 = utils.addPadString(parts[2], '0', 8);
-            const part3 = utils.addPadString(parts[3], '0', 3);
-            const originalAccount = part0 + part1 + part2 + part3;
-            bban = `${formattedBankCode}0${originalAccount}182700`;
-        } else {
-            bban = utils.generateBbanForStandardDepositNumbers(deposit, formattedBankCode);
-        }
-        return utils.generateIbanFromBban(bban);
+  convertDepositToIban(deposit: string): string {
+    let formattedBankCode = this.bankCode;
+    // @ts-ignore:
+    if (formattedBankCode.startsWith('0')) {
+      formattedBankCode = formattedBankCode.replace('0', '');
     }
+    let bban;
+    const parts = deposit.split('-');
+    if (parts.length !== 4) throw new Error('Invalid deposit');
+    const part0 = utils.addPadString(parts[0], '0', 4);
+    const part1 = utils.addPadString(parts[1], '0', 3);
+    const part2 = utils.addPadString(parts[2], '0', 8);
+    const part3 = utils.addPadString(parts[3], '0', 3);
+    const originalAccount = part0 + part1 + part2 + part3;
+    bban = `${formattedBankCode}0${originalAccount}182700`;
+    return utils.generateIbanFromBban(bban);
+  }
 
-    isIbanFromThisBank(iban: string) : boolean {
-        console.log('iban : ', iban);
-        console.log('bankCode : ', this.bankCode);
-        return isSourceOfIbanIsValid(iban, this.bankCode);
-    }
+  isIbanFromThisBank(iban: string): boolean {
+    console.log('iban : ', iban);
+    console.log('bankCode : ', this.bankCode);
+    return isSourceOfIbanIsValid(iban, this.bankCode);
+  }
 };
