e6320c01f8d74375678087115c78f71dc4e2f6ab
renjer <mranjbar.z2993@gmail.com>
2018-12-11 19:40:40 +0330

    Add decentralized deposits for sepah

--- lib/banks/sepah.ts
+++ lib/banks/sepah.ts
@@ -16,18 +16,27 @@ export class Sepah extends Bank{
             formattedBankCode = formattedBankCode.replace('0', '');
         }
         let bban;
-        if (deposit.indexOf('-') > -1) {
-            const parts = deposit.split('-');
-            const part0 = utils.addPadString(parts[0], '0', 8);
-            const part1 = utils.addPadString(parts[0], '0', 10);
-            const originalAccount = part0 + part1;
-            bban = `${formattedBankCode}1${originalAccount}182700`;
+        if (deposit.includes('-') ) {
+            const parts = deposit.split('-')
+           return this.convertDecentralizedDepositToIban(parts[0], parts[1])
         } else {
             bban = utils.generateBbanForStandardDepositNumbers(deposit, formattedBankCode);
         }
         return utils.generateIbanFromBban(bban);
     }
 
+    convertDecentralizedDepositToIban(deposit :string, branch:string):string{
+        let formattedBankCode = this.bankCode;
+        // @ts-ignore:
+        if (formattedBankCode.startsWith('0')) {
+            formattedBankCode = formattedBankCode.replace('0', '');
+        }
+        const branchPart = utils.addPadString(branch, '0', 8);
+        const depositPart = utils.addPadString(deposit, '0', 10);
+        const bban = `${formattedBankCode}1${branchPart}${depositPart}182700`;
+        return utils.generateIbanFromBban(bban);
+
+    }
     isIbanFromThisBank(iban: string): boolean {
         return  this.isIbanValid(iban) && isSourceOfIbanIsValid(iban, bankCodes.SEPAH_CODE);
 
--- test/banks/sepahTest.js
+++ test/banks/sepahTest.js
@@ -6,6 +6,35 @@ describe('testing convertSepahDepositToIban(deposit) function ', function() {
     const iban = 'IR840150000001426304971108'
     assert.equal(sepah.convertDepositToIban(deposit), iban);
   });
+
+  it('Should calculate iban correctly', () => {
+    const deposit = '1177301920207'
+    const iban = 'IR240150000001177301920207'
+    assert.equal(sepah.convertDepositToIban(deposit), iban);
+  });
+  it('Should calculate iban correctly', () => {
+    const deposit = '1177301920207'
+    const iban = 'IR240150000001177301920207'
+    assert.equal(sepah.convertDepositToIban(deposit), iban);
+  });
+  it('Should calculate iban correctly', () => {
+    const deposit = '1351800087201'
+    const iban = 'IR930150000001351800087201'
+    assert.equal(sepah.convertDepositToIban(deposit), iban);
+  });
+  it('convertDecentralizedDepositToIban Should calculate iban correctly', () => {
+    const deposit = '148986'
+    const iban = 'IR680151000001920000148986'
+    const  branch = '192'
+    assert.equal(sepah.convertDecentralizedDepositToIban(deposit, branch), iban);
+  });
+
+  it('convertDecentralizedDepositToIban Should calculate iban correctly', () => {
+    const deposit = '188852'
+    const iban = 'IR340151000020000000188852'
+    const  branch = '2000'
+    assert.equal(sepah.convertDecentralizedDepositToIban(deposit, branch), iban);
+  });
 });
 
 
