7fa5f629ad7c4181adc89a527f94f5cac6f25e29
renjer <mranjbar.z2993@gmail.com>
2019-04-20 09:27:57 +0430

    Improve isIbanValid test cases

--- test/general/utilsTest.js
+++ test/general/utilsTest.js
@@ -3,28 +3,6 @@ const utils = require('../../dist/index').util
 
 const depositCardIbanData = require('../depositCardIbanData')
 
-
-
-describe('testing isValidIban function sepah iban ', function() {
-    it('iban is valid', () => {
-        const iban = 'IR840150000001426304971108'
-        assert.equal(utils.isValidIban(iban), true);
-    });
-});
-describe('testing isValidIban function sepah iban', function() {
-    it('iban is valid', () => {
-        const iban = 'IR840150000001426304971108'
-        assert.equal(utils.isValidIban(iban), true);
-    });
-});
-
-describe('testing isValidIban function mellat iban', function() {
-    it('iban is invalid', () => {
-        const iban = 'IR690120010000004168450796'
-        assert.equal(utils.isValidIban(iban), true);
-    });
-});
-
 describe('testing isValidIban function mellat iban', function() {
     it('iban is invalid', () => {
         const iban = 'IR680120010000004168450796'
@@ -33,19 +11,19 @@ describe('testing isValidIban function mellat iban', function() {
 });
 
 
-describe('testing isValidIban function ayande iban', function() {
-    it('iban is invalid', () => {
-        const iban = 'IR070620000000202057838008'
-        assert.equal(utils.isValidIban(iban), true);
-    });
-});
-
 describe('getBankCodeFromCardNumber', function() {
-
     depositCardIbanData.forEach(item => {
         it(`bank ${item.bankName}` , () => {
             assert.equal(utils.getBankCodeFromCardNumber(item.cardNumber), item.bankCode);
         });
     })
 
+});
+
+describe('isIbanValid', function() {
+    depositCardIbanData.forEach(item => {
+        it(`bank ${item.bankName}` , () => {
+            assert.equal(utils.isValidIban(item.iban), true);
+        });
+    })
 });
