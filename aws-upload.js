8329e5290f0a390f34fd035e23339757c7b01257
renjer <mranjbar.z2993@gmail.com>
2019-12-25 07:15:03 +0330

    Remove serverless, and completing aws-upload

--- aws-upload.js
+++ aws-upload.js
@@ -1,61 +1,62 @@
 const AWS = require("aws-sdk"); // from AWS SDK
 const fs = require("fs"); // from node.js
 const path = require("path"); // from node.js
-
+const rootFolderName = 'dist'
 // configuration
 const config = {
     s3BucketName: process.env.BUCKET_NAME,
-    folderPath: './dist' // path relative script's location
+    folderPath: `./${rootFolderName}` // path relative script's location
 };
 
 // initialize S3 client
 const s3Config = {
     signatureVersion: 'v4',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey:  process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 }
 const s3 = new AWS.S3(s3Config);
-console.log('s3 config ',s3Config)
+console.log('s3 config ', s3Config)
 // resolve full folder path
 const distFolderPath = path.join(__dirname, config.folderPath);
 
-// get of list of files from 'dist' directory
-fs.readdir(distFolderPath, (err, files) => {
+uploadDirectoryFiles(distFolderPath)
 
-    if(!files || files.length === 0) {
+function uploadDirectoryFiles(distFolderPath) {
+    const files = fs.readdirSync(distFolderPath)
+    if (!files || files.length === 0) {
         console.log(`provided folder '${distFolderPath}' is empty or does not exist.`);
         console.log('Make sure your project was compiled!');
         return;
     }
-
-    // for each file in the directory
     for (const fileName of files) {
-
         // get the full path of the file
         const filePath = path.join(distFolderPath, fileName);
-
         // ignore if directory
         if (fs.lstatSync(filePath).isDirectory()) {
+            uploadDirectoryFiles(filePath)
             continue;
         }
-
         // read file contents
-        fs.readFile(filePath, (error, fileContent) => {
-            // if unable to read file contents, throw exception
-            if (error) { throw error; }
-
-            // upload file to S3
-            s3.putObject({
-                Bucket: config.s3BucketName,
-                Key: fileName,
-                Body: fileContent
-            }, (err,res) => {
-                if (err) {
-                    return console.log("Error uploading file ", err)
-                }
-                console.log(`Successfully uploaded '${fileName}'!`);
-            });
-
-        });
+        uploadFile(filePath, fileName)
     }
-});
+
+}
+
+function uploadFile(filePath, fileName) {
+    const relativeFilePath = `${__dirname}/${rootFolderName}/`
+    const fileKey = filePath.replace(relativeFilePath, '')
+    console.log({fileName, filePath, fileKey})
+    const fileContent = fs.readFileSync(filePath)
+    // upload file to S3
+    s3.putObject({
+        Bucket: config.s3BucketName,
+        Key: fileKey,
+        Body: fileContent
+    }, (err, res) => {
+        if (err) {
+            return console.log("Error uploading file ", err)
+        }
+        console.log(`Successfully uploaded '${fileKey}'!`, {res});
+    });
+
+}
